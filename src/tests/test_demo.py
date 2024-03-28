4c00c1fd74e5c208d07292b3047e1d5bd9b08eb1
MariosNatsios <m.natsios@schoox.com>
2024-03-28 22:26:14 +0200

    Remove test_demo.py

--- src/tests/test_demo.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src.conf import CONF
-from src.fixtures import Fixtures
-
-
-class Test_Demo(Fixtures):
-    
-    def test_1(self):
-        CONF.driver.get("https://pm-tool-e63fa77e3353.herokuapp.com/")
